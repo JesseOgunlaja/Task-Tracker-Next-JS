c925d3e8ce828a90e0cbda202066f083bdc33d6c
Jesse Ogunlaja <jesseogunlaja@gmail.com>
2023-09-05 21:55:51 +0100

    Attempting to fix bug/ Trying to see where bug is

--- components/ClientOverallNav.tsx
+++ components/ClientOverallNav.tsx
@@ -1,15 +1,26 @@
 "use client";
 
+import { usePathname } from "next/navigation";
+import React from "react";
 import SignedInNavbar from "./SignedInNavbar";
 import Navbar from "./Navbar";
 
 const ClientOverallNav = ({ signedIn }: any) => {
-  const showBar = true;
+  const pathname = usePathname();
+
+  const showBar = false;
   return (
     <div>
       {" "}
       {!showBar && (
-        <>{signedIn ? <SignedInNavbar pathname={"/"} /> : <Navbar />}</>
+        <>
+          {signedIn ||
+          (pathname.includes("/projects") && !pathname.includes("/logIn")) ? (
+            <SignedInNavbar pathname={pathname} />
+          ) : (
+            <Navbar />
+          )}
+        </>
       )}
     </div>
   );
