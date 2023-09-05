3cd9ae9ef23f877b491a0eee2a8e69677c7aa17e
Jesse Ogunlaja <jesseogunlaja@gmail.com>
2023-09-05 22:12:12 +0100

    Attempting to fix bug

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
   try {
-    cookies().delete("token");
+    cookies().set("token", "tasty");
     return NextResponse.redirect(new URL("/logIn", request.url));
   } catch (err) {
     return NextResponse.json({ error: `${err}` }, { status: 500 });
