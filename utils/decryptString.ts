4c31c064216d32b4cff60d642473ddc43f9f15e3
Jesse Ogunlaja <jesseogunlaja@gmail.com>
2023-11-02 15:21:02 +0000

    Upgrade to next 14.0.1

--- utils/decryptString.ts
+++ utils/decryptString.ts
@@ -6,7 +6,7 @@ const ENCRYPTION_SERVER = process.env.ENCRYPTION_KEY;
 export function decryptString(nameGiven: String, client: boolean) {
   const decrypted = CryptoJS.AES.decrypt(
     nameGiven,
-    client ? ENCRYPTION_CLIENT : ENCRYPTION_SERVER,
+    client ? ENCRYPTION_CLIENT : ENCRYPTION_SERVER
   ).toString(CryptoJS.enc.Utf8);
   const parsed = JSON.parse(decrypted);
   return parsed.nameGiven;
